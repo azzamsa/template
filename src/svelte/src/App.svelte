7acfc6f06b982075e2bc0f510ceedc5e812961b1
azzamsa <me@azzamsa.com>
2022-11-03 23:07:46 +0700

    fix(svelte): warnings

--- src/svelte/src/App.svelte
+++ src/svelte/src/App.svelte
@@ -5,10 +5,10 @@
 
 <main class="flex flex-col text-center text-black">
   <div class="mt-60 flex justify-center space-x-10">
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
       <img class="h-24" src="/vite.svg" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank">
+    <a href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
       <img class="h-24" src={svelteLogo} alt="Svelte Logo" />
     </a>
   </div>
@@ -24,8 +24,11 @@
   </div>
 
   <p class="mt-12">
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the
-    official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noopener noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
   <p class="mt-5 text-gray-500">Click on the Vite and Svelte logos to learn more</p>
